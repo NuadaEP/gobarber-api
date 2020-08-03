35ef90ddc68f1124cb289577cffab92284d2211b
Bruno Cardoso <bruno@liveonsolutions.com>
2020-08-03 13:38:25 -0300

    add eslint and preattier configs

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,7 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "hi" });
-});
+app.get('/', (request, response) => response.json({ message: 'hi' }));
 
-app.listen(3333, () => console.log("Server started on port 3333"));
+app.listen(3333, () => console.log('Server started on port 3333'));
