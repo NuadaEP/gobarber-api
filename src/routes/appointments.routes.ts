1362564782ec2a72241cbbcfd7166707cf6725d1
Bruno Cardoso <bruno@liveonsolutions.com>
2020-08-03 16:33:42 -0300

    add models and class entity

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,16 +1,11 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
 import { startOfHour, parseISO, isEqual } from 'date-fns';
+
+import Appointment from '../models/Appointment';
 
 const appointmentsRouter = Router();
 
-interface AppointmentsInterface {
-  id: string;
-  provider: string;
-  date: Date;
-}
-
-const appointments: Array<AppointmentsInterface> = [];
+const appointments: Array<Appointment> = [];
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
@@ -27,11 +22,7 @@ appointmentsRouter.post('/', (request, response) => {
       .json({ message: 'This appointment is already booked' });
   }
 
-  const appointment = {
-    id: uuid(),
-    provider,
-    date: parsedDate,
-  };
+  const appointment = new Appointment(provider, parsedDate);
 
   appointments.push(appointment);
 
