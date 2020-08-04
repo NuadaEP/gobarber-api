95e4e6091362c72557b5d64b934b65d7ba0e06b8
Bruno Cardoso <bruno@liveonsolutions.com>
2020-08-04 12:54:11 -0300

    add repository folder

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,32 +1,25 @@
 import { Router } from 'express';
-import { startOfHour, parseISO, isEqual } from 'date-fns';
+import { startOfHour, parseISO } from 'date-fns';
 
-import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../models/AppointmentsRepository';
 
 const appointmentsRouter = Router();
-
-const appointments: Array<Appointment> = [];
+const appointmentsRepository = new AppointmentsRepository();
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
   const parsedDate = startOfHour(parseISO(date));
 
-  const findAppointInSameDate = appointments.find(appointment =>
-    isEqual(parsedDate, appointment.date),
-  );
-
-  if (findAppointInSameDate) {
+  if (appointmentsRepository.findByDate(parsedDate)) {
     return response
       .status(400)
       .json({ message: 'This appointment is already booked' });
   }
 
-  const appointment = new Appointment(provider, parsedDate);
+  const appointment = appointmentsRepository.create(provider, parsedDate);
 
-  appointments.push(appointment);
-
-  return response.json(appointments);
+  return response.json(appointment);
 });
 
 export default appointmentsRouter;
