857ef239ed78f31d7977d8926bb3c7855da24e70
Bruno Cardoso <bruno@liveonsolutions.com>
2020-09-15 16:24:18 -0300

    add foreign key

--- src/database/migrations/1600094038759-CreateAppointments.ts
+++ src/database/migrations/1600094038759-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1600094038759
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
