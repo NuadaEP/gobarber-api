7adceacd4ddd5f62ec3ac42bd3e328b5aef794ce
Bruno Cardoso <bruno@liveonsolutions.com>
2020-09-24 18:30:16 -0300

    ignore vars

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+
 import Routes from './routes';
 import uploadConfig from './config/upload';
 import AppError from './errors/AppError';
@@ -13,22 +15,20 @@ app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(Routes);
 
-app.use(
-  (err: Error, request: Request, response: Response, next: NextFunction) => {
-    if (err instanceof AppError) {
-      return response.status(err.statusCode).json({
-        status: 'error',
-        message: err.message,
-      });
-    }
-
-    console.error(err);
-
-    return response.status(500).json({
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
       status: 'error',
-      message: 'Internal Server Error',
+      message: err.message,
     });
-  },
-);
+  }
+
+  // console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error',
+  });
+});
 
 app.listen(3333, () => console.log('Server started on port 3333'));
