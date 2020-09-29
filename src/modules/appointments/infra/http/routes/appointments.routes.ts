84218af57df20d48005ca1ede314f9ccafbeb557
Bruno Cardoso <bruno@liveonsolutions.com>
2020-09-29 17:38:17 -0300

    add controller into appointments module

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,37 +1,13 @@
 import { Router } from 'express';
-import { parseISO } from 'date-fns';
-import { container } from 'tsyringe';
 
-import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 import endureAuthenticated from '@modules/users/infra/http/middlewares/endureAuthenticated';
+import AppointmentController from '../controllers/AppointmentsController';
 
 const appointmentsRouter = Router();
+const appointmentsController = new AppointmentController();
 
 appointmentsRouter.use(endureAuthenticated);
 
-// appointmentsRouter.get('/', async (request, response) => {
-//   const appointments = await appointmentsRepository.find();
-
-//   return response.json(appointments);
-// });
-
-appointmentsRouter.post('/', async (request, response) => {
-  try {
-    const { provider_id, date } = request.body;
-
-    const parsedDate = parseISO(date);
-
-    const createAppointment = container.resolve(CreateAppointmentService);
-
-    const appointment = await createAppointment.execute({
-      date: parsedDate,
-      provider_id,
-    });
-
-    return response.json(appointment);
-  } catch (error) {
-    return response.status(error.statusCode).json({ error: error.message });
-  }
-});
+appointmentsRouter.post('/', appointmentsController.create);
 
 export default appointmentsRouter;
