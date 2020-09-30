b29ffffb831732034f5a6dd53c13d475d8c80809
Bruno Cardoso <bruno@liveonsolutions.com>
2020-09-30 17:58:03 -0300

    add user test

---
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -0,0 +1,33 @@
+import FakeUserRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import CreateUserService from '@modules/users/services/CreateUserService';
+import AppError from '@shared/errors/AppError';
+
+describe('CreateUser', () => {
+  it('should be able to create a new user', async () => {
+    const fakeUsersRepository = new FakeUserRepository();
+    const createUser = new CreateUserService(fakeUsersRepository);
+
+    const user = await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    expect(user).toHaveProperty('id');
+  });
+
+  it('should no be able to create a new user with a same email', async () => {
+    const fakeUsersRepository = new FakeUserRepository();
+    const createUser = new CreateUserService(fakeUsersRepository);
+
+    const data = {
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    };
+
+    await createUser.execute(data);
+
+    expect(createUser.execute(data)).rejects.toBeInstanceOf(AppError);
+  });
+});
