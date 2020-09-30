64ad9f5b630a36993f6feeeffb3719d845a89703
Bruno Cardoso <bruno@liveonsolutions.com>
2020-09-30 17:09:51 -0300

    adding the error test

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,5 +1,6 @@
 import FakeAppointmentRepository from '@modules/appointments/repositories/fakes/FakeAppointmentRepository';
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
+import AppError from '@shared/errors/AppError';
 
 describe('CreateAppointment', () => {
   it('should be able to create a new appointment', async () => {
@@ -17,7 +18,24 @@ describe('CreateAppointment', () => {
     expect(appointment.provider_id).toBe('123456');
   });
 
-  // it('should not be able to create two appointments on the same time', () => {
+  it('should not be able to create two appointments on the same time', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentRepository();
+    const createAppotiment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
 
-  // })
+    const appointmentDate = new Date(2020, 4, 10, 11);
+
+    await createAppotiment.execute({
+      provider_id: '123456',
+      date: appointmentDate,
+    });
+
+    expect(
+      createAppotiment.execute({
+        provider_id: '123456',
+        date: appointmentDate,
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
